435438209ce071d118e8039cc8ebcdf0a3cb080f
정진규 <rritjy@naver.com>
2023-10-17 17:55:41 +0900

    10/17 작업

---
+++ HelloWorldLightningWebComponent/force-app/main/default/lwc/accUpdateModal/accUpdateModal.js
@@ -0,0 +1,81 @@
+import { api } from 'lwc';
+import LightningModal from 'lightning/modal';
+
+import ID_FIELD from "@salesforce/schema/Account.Id";
+import NAME_FIELD from "@salesforce/schema/Account.Name";
+import TYPE_FIELD from "@salesforce/schema/Account.Type";
+import PHONE_FIELD from "@salesforce/schema/Account.PHONE";
+import WEBSITE_FIELD from "@salesforce/schema/Account.WEBSITE";
+
+// 세일즈포스 내 업데이트 할 수 있는 메서드
+import { updateRecord } from "lightning/uiRecordApi";
+
+export default class AccUpdateModal extends LightningModal {
+  @api recordData
+  
+  accId = '';
+  accName = '';
+  accType = '';
+  accPhone = '';
+  accWebsite = '';
+  
+
+  connectedCallback() {
+    if (this.recordData) {
+      // 해당 레코드 데이터가 존재하지 않으면 ''으로
+      this.accId = this.recordData.Id || '';
+      this.accName = this.recordData.Name || '';
+      this.accType = this.recordData.Type || '';
+      this.accPhone = this.recordData.Phone || '';
+      this.accWebsite = this.recordData.Website || '';
+    }
+  }
+
+   // 계정 타입 콤보박스 옵션
+  accTypeOptions = [
+    { label: 'Prospect', value: 'Prospect' },
+    { label: 'Customer - Direct', value: 'Customer - Direct' },
+    { label: 'Customer - Channel', value: 'Customer - Channel' },
+    { label: 'Channel Partner / Reseller', value: 'Channel Partner / Reseller' },
+    { label: 'Installation Partner', value: 'Installation Partner' },
+    { label: 'Technology Partner', value: 'Technology Partner' },
+    { label: 'Other', value: 'Other' }
+  ];
+
+  handleAccNameChange(e){this.accName = e.detail.value;}
+  handleAccTypeChange(e){this.accType = e.detail.value;}
+  handleAccPhoneChange(e){this.accPhone = e.detail.value;}
+  handleAccWebsiteChange(e){this.accWebsite = e.detail.value;}
+
+  handleClosedModal() {
+    console.log('모달창 닫힘');
+    this.close();
+  }
+
+  handleUpdate(){
+    console.log(this.accId);
+    console.log(this.accName);
+    console.log(this.accType);
+    console.log(this.accPhone);
+    console.log(this.accWebsite);
+    // this.template.querySelector('lightning-record-edit-form').submit();
+
+    const fields = {};
+
+    fields[ID_FIELD.fieldApiName] = this.accId;
+    fields[NAME_FIELD.fieldApiName] = this.accName;
+    fields[TYPE_FIELD.fieldApiName] = this.accType;
+    fields[PHONE_FIELD.fieldApiName] = this.accPhone;
+    fields[WEBSITE_FIELD.fieldApiName] = this.accWebsite;
+        
+    const recordInput = {
+      fields: fields
+    };
+
+    updateRecord(recordInput).then((record) => {
+      console.log(record);
+    });
+
+    this.handleClosedModal();
+  }
+}
--- HelloWorldLightningWebComponent/force-app/main/default/lwc/tagTableCustom/tagTableCustom.js
+++ HelloWorldLightningWebComponent/force-app/main/default/lwc/tagTableCustom/tagTableCustom.js
@@ -1,5 +1,7 @@
 import { LightningElement, track } from 'lwc';
 import getAccounts  from '@salesforce/apex/TagTableCustom.getAccounts';
+import getAccountsCount from '@salesforce/apex/TagTableCustom.getAccountsCount';
+import AccUpdateModal from 'c/accUpdateModal';
 
 const columns = [
   {
@@ -16,12 +18,51 @@ const columns = [
   { label: '유형', fieldName: 'Type' },
   { label: '전화번호', fieldName: 'Phone', type: 'Phone' },
   { label: '웹사이트', fieldName: 'Website'},
-  { label: '계정 소유자', fieldName: 'OwnerId'}
+  { label: '계정 소유자', fieldName: 'Owner.Name'}
 ];
 
 export default class TagTableCustom extends LightningElement {
   @track data;
   columns = columns;
+  isLoading = false;
+  totalLeadCount = 0; // 전체 데이터의 개수
+  currentPage = 1; // 현재 페이지
+  pageSize = 10; // 한 페이지당 표시할 데이터 개수
+
+  handleOpenClick(event) {
+    // id가 하이퍼링크라 '/'를 없앰
+    const recordIdParts = event.target.dataset.recordid.split('/');
+    const extractedId = recordIdParts[recordIdParts.length - 1];
+
+    // 레코드 정보를 생성
+    const recordData = {
+        Id: extractedId,
+        Name: event.target.dataset.name,
+        Type: event.target.dataset.type,
+        Phone: event.target.dataset.phone,
+        Website: event.target.dataset.website
+    };
+
+    // 모달 창 열기 및 레코드 정보 전달
+    AccUpdateModal.open({
+        size: 'large',
+        recordData: recordData // 레코드 정보 전달
+    }).then((result) => {
+        console.log(result);
+    });
+  }
+
+  // 추가 데이터 로드 및 무한 스크롤 처리
+  handleScroll(event) {
+    const container = event.target;
+    const containerHeight = container.clientHeight;
+    const scrollPosition = container.scrollTop;
+    const scrollHeight = container.scrollHeight;
+
+    if (containerHeight + scrollPosition >= scrollHeight - 1) {
+      this.retrieveData();
+    }
+  }
 
   // DOM연결
   connectedCallback() {
@@ -30,10 +71,20 @@ export default class TagTableCustom extends LightningElement {
 
   loadData(){
     this.retrieveData();
+    this.retrieveTotalLeadCount();
   }
 
   retrieveData(){
-    getAccounts()
+    if (this.data && this.data.length >= this.totalLeadCount) {
+      return false;
+    }
+
+    this.isLoading = true; // 데이터를 가져오는 동안 로딩 표시 활성화
+
+    getAccounts({
+      pageNumber: this.currentPage,
+      pageSize: this.pageSize
+    })
     .then(result => {
       if (result && result.length > 0) {
         this.data = [...(this.data || []), ...result.map(record => ({
@@ -41,6 +92,7 @@ export default class TagTableCustom extends LightningElement {
           Id: '/' + record.Id
         }))];
         this.error = undefined;
+        this.currentPage++;
       } else {
         this.error = undefined;
         this.data = undefined;
@@ -49,6 +101,19 @@ export default class TagTableCustom extends LightningElement {
     .catch(error => {
       this.error = error;
       this.data = undefined;
+    })
+    .finally(() => {
+      this.isLoading = false; // 데이터 로드가 완료되면 로딩 표시 비활성화
     });
+  }
+
+  retrieveTotalLeadCount() {
+    getAccountsCount()
+      .then(result => {
+        this.totalLeadCount = result;
+      })
+      .catch(error => {
+        this.totalLeadCount = 0;
+      });
   }
 }
